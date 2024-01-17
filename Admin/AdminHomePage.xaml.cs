631802c09647108c19a64d5285be50011794e4c9
dimavoronkov2222 <dimavoronkov2222@hotmail.com>
2024-01-17 08:49:16 +0100

    AdminHomePage Update

--- Admin/AdminHomePage.xaml.cs
+++ Admin/AdminHomePage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Acadamy.Student;
 using Acadamy.Teacher;
+using Microsoft.Data.SqlClient;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -27,20 +28,52 @@ namespace Acadamy.Admin
         {
             InitializeComponent();
             listschool.ItemsSource = SharedData.Instance.ListBoxItems;
-
         }
+        private string connectionS = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=AcadamyDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             var selectedItem = dayOfWeekComboBox.SelectedItem as ComboBoxItem;
-            string selectedText = selectedItem != null ? selectedItem.Content.ToString() : string.Empty;
-            string textBoxContent = textBox.Text;
-            if (!string.IsNullOrEmpty(selectedText) && !string.IsNullOrEmpty(textBoxContent))
+            string selectedDayOfWeek = selectedItem != null ? selectedItem.Content.ToString() : string.Empty;
+            string timeSlot = textBox.Text;
+            string courseName = Coursename.Text;
+            string teacherName = "Teacher";
+            int yourScheduleID = 1;
+            if (!string.IsNullOrEmpty(selectedDayOfWeek) && !string.IsNullOrEmpty(timeSlot))
             {
-                SharedData.Instance.ListBoxItems.Add($"{selectedText}: {textBoxContent}");
+                string queryCheck = "SELECT COUNT(*) FROM Schedule WHERE ScheduleID = @ScheduleID";
+                using (SqlCommand commandCheck = new SqlCommand(queryCheck, new SqlConnection(connectionS)))
+                {
+                    commandCheck.Parameters.AddWithValue("@ScheduleID", yourScheduleID);
+                    int existingCount = (int)commandCheck.ExecuteScalar();
+                    if (existingCount > 0)
+                    {
+                        yourScheduleID += 1;
+                        commandCheck.Parameters["@ScheduleID"].Value = yourScheduleID;
+                        existingCount = (int)commandCheck.ExecuteScalar();
+                    }
+                    string query = "SET IDENTITY_INSERT Schedule ON; INSERT INTO Schedule (ScheduleID, DayOfWeek, TimeSlot, CourseName, TeacherName) VALUES (@ScheduleID, @DayOfWeek, @TimeSlot, @CourseName, @TeacherName); SET IDENTITY_INSERT Schedule OFF;";
+                    using (SqlCommand command = new SqlCommand(query, new SqlConnection(connectionS)))
+                    {
+                        command.Parameters.AddWithValue("@ScheduleID", yourScheduleID);
+                        command.Parameters.AddWithValue("@DayOfWeek", selectedDayOfWeek);
+                        command.Parameters.AddWithValue("@TimeSlot", timeSlot);
+                        command.Parameters.AddWithValue("@CourseName", courseName);
+                        command.Parameters.AddWithValue("@TeacherName", teacherName);
+                        int rowsAffected = command.ExecuteNonQuery();
+                        if (rowsAffected > 0)
+                        {
+                            MessageBox.Show("Data saved successfully");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Failed to save data");
+                        }
+                    }
+                }
             }
             else
             {
-                MessageBox.Show("Please select an item and enter text");
+                MessageBox.Show("Please select a day of the week and enter a time slot");
             }
         }
     }
